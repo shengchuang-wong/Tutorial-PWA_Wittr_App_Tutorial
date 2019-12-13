c1cf89ca7735fae1c19675a39e794dc2999c4bc6
shengchuang-wong <shengchuang1996@gmail.com>
2019-12-13 17:43:10 +0800

    initial cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -31,5 +31,31 @@
 //     }).catch(error => {
 //       return new Response('Opps, something went wrong.')
 //     })
-//   )
+//   )ic
 // })
+
+self.addEventListener('install', (event) => {
+
+  event.waitUntil(
+    caches.open('wittr-static-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  )
+
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if(response) return response
+      return fetch(event.request)
+    })
+  )
+})
